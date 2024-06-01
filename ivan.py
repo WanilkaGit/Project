98408b0a75e753ba26a5a277cca3771c886ac992
WanilkaGit <130846873+WanilkaGit@users.noreply.github.com>
2024-06-01 22:42:51 +0300

    Update ivan.py
    Список карти готовий

--- ivan.py
+++ ivan.py
@@ -1,35 +1,22 @@
 map_lvlq1 = {
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
-  "||",
+  "||||||||||||||||||",#Unbreakeble - u
+  "|gggggggggggggggg|",#breakeable - b
+  "|gb bbbb  bbbb bg|",#green_hide - g
+  "|gb b  b  b  b bg|",#dark_white_hide - d
+  "|gb bbbb  b  b bg|",#enemy - e
+  "|gb    b  b  b bg|",#player1 - p
+  "|gb bbbb  bbbb  bg|",#lose - l
+  "|gb  bb    bb  bg|",#win - w
+  "|gb  bb bb bb  bg|",
+  "|gb    bbbb    bg|",
+  "|gggggggggggggggg|",
+  "|u     b b      u|",
+  "|g  b  bbb   b bg|",
+  "|gb b  b b   b bg|",
+  "|gb bu      ub bg|",
+  "|gb b  bbb   b bg|",
+  "|ggg   blb    gg |",
+  "||||||||||||||||||"
 }
+
+class BreakableBlock():   

4d3c51c4e9e1e729ff926ad30e8c1dd981939d3f
WanilkaGit <130846873+WanilkaGit@users.noreply.github.com>
2024-06-02 01:11:57 +0300

    Update ivan.py

--- ivan.py
+++ ivan.py
@@ -1,21 +1,21 @@
 map_lvlq1 = {
-  "\_________________",#Unbreakeble - u
-  "\gggggggggggggggg/",#breakeable - b
-  "\gb bbbb  bbbb bg/",#green_hide - g
-  "\gb b  b  b  b bg/",#dark_white_hide - d
-  "\gb bbbb  b  b bg/",#enemy - e
-  "\gb    b  b  b bg/",#player1 - p
-  "\gb bbbb  bbbb bg/",#lose - l
-  "\gb  bb    bb  bg/",#win - w
-  "\gb  bb bb bb  bg/",
-  "\gb    bbbb    bg/",
-  "\gggggggggggggggg/",
-  "\u     b b      u/",
-  "\g  b  bbb   b bg/",
-  "\gb b  b b   b bg/",
-  "\gb bu      ub bg/",
-  "\gb b  bbb   b bg/",
-  "\ggg   blb    gg /",
+  "_________________",#Unbreakeble - u
+  "|gggggggggggggggg/",#breakeable - b
+  "|gb bbbb  bbbb bg/",#green_hide - g
+  "|gb b  b  b  b bg/",#dark_white_hide - d
+  "|gb bbbb  b  b bg/",#enemy - e
+  "|gb    b  b  b bg/",#player1 - p
+  "|gb bbbb  bbbb bg/",#lose - l
+  "|gb  bb    bb  bg/",#win - w
+  "|gb  bb bb bb  bg/",
+  "|gb    bbbb    bg/",
+  "|gggggggggggggggg/",
+  "|u     b b      u/",
+  "|g  b  bbb   b bg/",
+  "|gb b  b b   b bg/",
+  "|gb bu      ub bg/",
+  "|gb b  bbb   b bg/",
+  "|ggg   blb    gg /",
   "------------------"
 }
 
@@ -25,13 +25,6 @@ def start_pos():# стартова позиція
     global items, camera, hero, block_r, block_l, plat, coins, door, coin
     global stairs_lst, enemy_lst, p6, p11, p7, p8, open_d, open_ch, manas# робимо глобальними змінни
     hero = Player(300, 650, 50, 50 , 15, hero_l)
-    camera = Camera(camera_config, level1_width, level1_height)# створюєма наблюдателя
-    
-    items = sprite.Group()#  створюємо тусу
-    manas = sprite.Group()
-
-    open_d = False
-    open_ch = False
 
 
     block_r = []# список 1:

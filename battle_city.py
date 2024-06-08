3cc23242ae2354d33ef4297338c0520bf2205df5
Abobus23828 <demanenkooleksij4@gmail.com>
2024-06-08 08:43:57 +0300

    Update battle_city.py

--- battle_city.py
+++ battle_city.py
@@ -1,16 +1,26 @@
 import pygame as pg
 
 pg.init()
+pg.mixer.init()
 
-FPS = 60
+
+hero_lives = 1
+
+
+screen = pg.display.set_mode()
+font = pg.font.SysFont('TH SarabunPSK Bold', 65, True, False) 
+text_game_over = font.render("You lose", True, (51, 225, 249)) 
+text_game_over1 = font.render("Press ESC to restart", True, (51, 225, 249)) 
+
 
 display = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 pg.display.set_caption('Battle City')
+pg.mixer.music.load('lose.ogg')
 
 game = True
 
-clock = pg.time.Clock()
 while game:
+    
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game = False
@@ -18,9 +28,18 @@ while game:
             if event.key == pg.K_ESCAPE:
                 game = False
 
+    if hero_lives == 0:
+        game = False
+    else:
+        pass
+    
+    if game == False:
+        screen.blit(text_game_over, [20, 170])
+        screen.blit(text_game_over1, [20, 210])
+        pg.mixer.music.play()
+    else:
+        pass
+
+
 
     pg.display.update()
-    clock.tick(FPS)
-    
-
-pg.quit()

15457da86192b4ecb9389a7a77fea1c041eec02e
max <ms20090806@gmail.com>
2024-06-09 18:17:43 +0200

    повернув все як було

--- battle_city.py
+++ battle_city.py
@@ -1,21 +1,11 @@
 import pygame as pg
 
 pg.init()
-pg.mixer.init()
-
-
-hero_lives = 1
-
-
-screen = pg.display.set_mode()
-font = pg.font.SysFont('TH SarabunPSK Bold', 65, True, False) 
-text_game_over = font.render("You lose", True, (51, 225, 249)) 
-text_game_over1 = font.render("Press ESC to restart", True, (51, 225, 249)) 
 
 
 display = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 pg.display.set_caption('Battle City')
-pg.mixer.music.load('lose.ogg')
+
 
 game = True
 
@@ -28,17 +18,6 @@ while game:
             if event.key == pg.K_ESCAPE:
                 game = False
 
-    if hero_lives == 0:
-        game = False
-    else:
-        pass
-    
-    if game == False:
-        screen.blit(text_game_over, [20, 170])
-        screen.blit(text_game_over1, [20, 210])
-        pg.mixer.music.play()
-    else:
-        pass
 
 
 

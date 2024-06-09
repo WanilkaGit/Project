9ccbd4ecb1ee101790cec8569e8e4fbcba09d991
KostyaNike <128311669+KostyaNike@users.noreply.github.com>
2024-06-09 12:52:31 +0000

    kostyatest

--- ivan.py
+++ ivan.py
@@ -1,4 +1,18 @@
 from pygame import *
+"""Класи"""
+class Settings(sprite.Sprite):# основний клас тут основні параметри
+    def __init__(self, x, y, width, height, speed, img):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.image = transform.scale(image.load(img), (self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def reset(self):# тут прописана функція ресет
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 """Map build"""
 map_lvl1 = {
@@ -20,92 +34,86 @@ map_lvl1 = {
     "|gb b  bbb   b bg/",
     "|ggg   blb    gg /",
     "------------------"
-}
+    }
 
-"""Картинки щоб швидше вставляти бо по іншому довго"""
+x = 0
+y = 0
+def start_pos():# функція що розставляє все по стартових місцях
+    global block_l, block_r, plat#все треба буде глобалізувати
+    hero = Player(300, 650, 50, 50 , 15, hero_l)
 
-hero_r = "images/hero_r.png"
-hero_l = "images/hero_l.png"
+    items = list()
 
-enemy_l = "images/enemy_l.png"
-enemy_r = "images/enemy_r.png"
+    block_r = []# список 1:
+    block_l = []# список 2:
+    plat = []# список 3:
+    
+    # всі списки дивіться в кінотеатрах(коді)
+    x = 0#  координати для обєктів
+    y = 0
+    for r in map_lvl1:# р дорівнює ряддку
+        for c in r:#  с дорівнює символу з рядка р
+            if c == "-":# дім полу
+                p1 = Settings(x,y, 40, 40, 0, "platform")# створюєм платформу спочатку координати, розмір, швидкість та картинка
+                plat.append(p1)# додаємо до списку платформ
+                items.add(p1)# додаємо до списку всього що є на карті
+            if c == "|":# для лівої стіни
+                p1 = Settings(x,y, 40, 40, 0, "platform")# створюєм раба платформа
+                plat.append(p1)# 
+                items.add(p1)
+            if c == "/":# для правої стіни
+                p1 = Settings(x,y, 40, 40, 0, "platform")# створюєм раба платформа
+                plat.append(p1)# 
+                items.add(p1)
 
-coin_img = "images/coin.png"
-door_img = "images/door.png"
-key_img = "images/key.png"
-chest_open = "images/cst_open.png"
-chest_close = "images/cst_close.png"
-stairs = "images/stair.png"
-portal_img = "images/portal.png"
-platform = "images/platform.png"
-power = "images/mana.png"
-nothing = "images/nothing.png"
-boss = "images/nothing.png"
+            if c == "g":# для трави
+                p1 = Settings(x,y, 40, 40, 0, "platform")# створюєм раба платформа
+                plat.append(p1)# 
+                items.add(p1)
 
+            if c == "b":# для кирпічної стіни
+                p1 = Settings(x,y, 40, 40, 0, "platform")# створюєм раба платформа
+                plat.append(p1)# 
+                items.add(p1)
 
+            if c == "u":# для не взламної стіни
+                p1 = Settings(x,y, 40, 40, 0, "platform")# створюєм раба платформа
+                plat.append(p1)# 
+                items.add(p1)
 
-h_m_c = 0
-font1 = font.SysFont("Arial", 35)
-font2 = font.SysFont(('font/ariblk.ttf'), 60)
-e_tap = font2.render('press (e)', True, (255, 0, 255))
-k_need = font2.render('You need a key to open!', True, (255, 0, 255))
-space = font2.render('press (space) to kill the enemy', True, (255, 0, 255))
-
-# це те скільки вийде блоків на екрані 40 кількість пікселів на оин силвол
-level1_width = len(level1[0]) * 40
-level1_height = len(level1) * 40
-
-#розміри екрану
-W = 1280
-H = 720
-
-
-
-# створюєм вікно
-window = display.set_mode((W, H))
-
-
-# Все для вікна
-back = transform.scale(image.load("images/bgr.png"), (W, H))# фон
-display.set_caption("Лошок")# назва
-display.set_icon(image.load("images/key.png"))# картинка біля назви
-
-
-
-"""Класи"""
-class Settings(sprite.Sprite):# основний клас тут основні параметри
-    def __init__(self, x, y, width, height, speed, img):
-        super().__init__()
-        self.width = width
-        self.height = height
-        self.speed = speed
-        self.image = tansform.scale(image.load(img), (self.width, self.height))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+            if c == "e":# для еміків
+                p1 = Settings(x,y, 40, 40, 0, "platform")# створюєм раба платформа
+                plat.append(p1)# 
+                items.add(p1)
 
 
 
-    def reset(self):# тут прописана функція ресет
-        window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(Settings):# клас гравця з супер класом сетінгс
-    def r_l(elf):# тут буде переміщення в право ліво
-        global mana
-        key_pressed = key.get_pressed()# задаєм в зміну значення
-        if key_pressed[K_a]:# перевіряєм чи нажата кнопка це а
-            self.rect.x -= self.speed# якщо так той демо в ліво
-            self.image = transform.scale(image.load(hero_l), (self.width, self.height))# підсьтавляєм фотку
-            kana.side = "left"
+            x += 40#  ікси плюс 40
+        y += 40#  перміщаємось в низ
+        x = 0#  ікси 0
+    items.add(hero)
 
-        if key_pressed[K_d]:#кнопка в низ натиснута
-            self.rect.x += self.speed# х додаєм швидкість рухаємось
-            self.image = transform.scale(image.load(hero_r), (self.width, self.height))#  підставляєм фотку
-            mana.side = "right
-            
-        if key_pressed[K_s]:# якщо в низ тобто в низ
-            self.rect.y += self.speed# ми додає тобто спускаємось
+"""Key bulding"""
 
-        if key_pressed[K_w]:# якщо в верх то віднімаєм піднімаємось
-            self.rect.y -= self.speed# 
+def keys_building():
+    key_pressed = key.get_pressed()
+    if key_pressed["K_a"]:
+        pass
+
+    if key_pressed["K_w"]:
+        pass
+
+    if key_pressed["K_s"]:
+        pass
+
+    if key_pressed["K_d"]:
+        pass
+
+    if key_pressed["K_SPACE"]:
+        pass
+
+    if key_pressed["K_ESCAPE"]:
+        pass
+
 

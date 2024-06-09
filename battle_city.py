cf27cebc6914469a2406b24a0388092e433c5e80
max <ms20090806@gmail.com>
2024-06-09 23:39:04 +0200

    прибрав за собой

--- battle_city.py
+++ battle_city.py
@@ -3,24 +3,12 @@ import pygame as pg
 pg.init()
 pg.mixer.init()
 
-
-hero_lives = 1
-
-
-screen = pg.display.set_mode()
-font = pg.font.SysFont('TH SarabunPSK Bold', 65, True, False) 
-text_game_over = font.render("You lose", True, (51, 225, 249)) 
-text_game_over1 = font.render("Press ESC to restart", True, (51, 225, 249)) 
-
-
 display = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 pg.display.set_caption('Battle City')
-pg.mixer.music.load('lose.ogg')
 
 game = True
 
 while game:
-    
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game = False

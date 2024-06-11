25c316deb5d599c6072cbaab2c255ee219f1e439
sonya1045 <130846963+sonya1045@users.noreply.github.com>
2024-06-11 10:40:03 +0300

    Update sonya.py

--- sonya.py
+++ sonya.py
@@ -1,10 +1,14 @@
 import pygame as pg
+from maxym import *
+from ivan import *
+
 
 pg.init()
 
 def main_menu():
+    global setting_btn, how_to_play_btn, play_btn
     font = pg.font.Font(None, 32)
-    back = pg.transform.scale(pg.image.load('fon1.jpg'), (1500, 1000))  #завантажуєемо картинку фона і розтягємо її у рзміри екрана
+   
     pg.display.set_icon(pg.image.load('fon1.jpg'))  #завантажуєемо фото іконки
     pg.display.set_caption('Battle city') #даємо назву вікну додатка
          ### об'єкти кнопок ###
@@ -12,20 +16,24 @@ def main_menu():
     play_btn = Button(630, 300, 200, 80, font, 'Play', (100, 10, 10))
     setting_btn = Button(630, 400, 200, 80, font, 'Settings', (100, 10, 10))
         # відмальовка об'єктів #
-    display.blit(back, (0,0))
+    how_to_play_btn.draw(window)
+    play_btn.draw(window)
+    setting_btn.draw(window)
 
-    how_to_play_btn.draw(display)
-    play_btn.draw(display)
-    setting_btn.draw(display)
-        ### змінні в яких зберігається стан кнопок(натиснута чи ні) ###
+    
+def push_btn():
     press_set = setting_btn.is_pressed([630, 400])
-    press_play = setting_btn.is_pressed([630, 300])
-    press_how_play = setting_btn.is_pressed([630, 200])
-    if press_set == True and event.type == pg.MOUSEBUTTONDOWN:
+    press_play = play_btn.is_pressed([630, 300])
+    press_how_play = how_to_play_btn.is_pressed([630, 200])
+
+    if press_set == True:
         setting()
 
-    if press_play == True and  event.type == pg.MOUSEBUTTONDOWN:
+    if press_play == True:
         start_pos()
 
-    if press_how_play == True and  event.type == pg.MOUSEBUTTONDOWN:
+    if press_how_play == True:
         pass
+
+def setting():
+    pass

86e24e7f9ce1e8a8c565d87fe3812a042989d29c
sonya1045 <130846963+sonya1045@users.noreply.github.com>
2024-06-11 10:30:32 +0300

    Починаю зводити увесь код сюди
    Змінила вікно display на window оскільки display системне ім'я, яке неможна використовувати для змінної. Вікно вже заупескається, але нажаль тільки меню працює, кнопки ще не всі працюють

--- battle_city.py
+++ battle_city.py
@@ -1,22 +1,28 @@
 import pygame as pg
+from maxym import *
+from ivan import *
+from sonya import main_menu, setting, push_btn
 
 pg.init()
 pg.mixer.init()
 
-display = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+window = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 pg.display.set_caption('Battle City')
-
+back = pg.transform.scale(pg.image.load('fon1.jpg'), (1500, 1000))  #завантажуєемо картинку фона і розтягємо її у рзміри екрана
 game = True
 
 while game:
+    window.blit(back, (0,0))
+    main_menu()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game = False
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 game = False
-
-
-
+                        
+        if event.type == pg.MOUSEBUTTONDOWN:
+            push_btn()
 
     pg.display.update()
+pg.quit()

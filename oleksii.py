5b33d4635dd65ad5657d0856f11dbccbfe2c73b6
sonya1045 <smikolaenko7@gmail.com>
2024-06-12 22:23:34 +0300

    update

--- oleksii.py
+++ oleksii.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 pg.init()
-pg.mixer.init()
+
 hero_lives = 3
 
 WHITE = (255, 255, 255)
@@ -9,13 +9,7 @@ WHITE = (255, 255, 255)
 screen = pg.display.set_mode()
 font = pg.font.SysFont('Aharoni', 65, True, False) 
 text_game_over = font.render("You lose", True, (51, 225, 249)) 
-text_game_over1 = font.render("Press ESC to restart", True, (51, 225, 249)) 
-
-
-display = pg.display.set_mode((0, 0), pg.FULLSCREEN)
-pg.display.set_caption('Battle City')
-pg.mixer.music.load('lose.ogg')
-
+text_game_over1 = font.render("Press r to restart", True, (51, 225, 249)) 
 
 def lose():
     screen.blit(text_game_over, [20, 170])

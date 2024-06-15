1455e3d41b33b61fa46bbda4d50e73e9855f55bd
sonya1045 <smikolaenko7@gmail.com>
2024-06-15 13:46:35 +0300

    update

--- oleksii.py
+++ oleksii.py
@@ -14,24 +14,13 @@ text_game_over = font.render("You lose", True, (51, 225, 249))
 
 text_life = font.render('Life: ' + str(hero_lives), True, (0,0,0))
 
-def lose(screen):
+def lose(window):
     global scene
     screen.blit(text_game_over, [20, 170])
-    screen.blit(text_game_over1, [20, 200])
     scene = 6
-    
-if hero_lives == 0:
-    game = False        
-else:
-    pass
-    
-if game == False:
-    window.blit(text_game_over, [600, 280])
-    pg.mixer.music.play()
     window.blit(restart_btn)
     window.blit(exit_to_main)
-else:
-    pass
+    
 
 
 

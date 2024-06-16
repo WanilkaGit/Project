115324b5466fb9879a9d38c230a46a2209cf91c2
KostyaNike <deras1234oo@gmail.com>
2024-06-16 13:21:29 +0200

    ыва

--- kostya.py
+++ kostya.py
@@ -2,14 +2,26 @@ import pygame as pg
 from maxym import TextureButton
 
 font = pg.font.Font(None, 36)
-back_button_from_htp = TextureButton(630, 300, 100, 100, "assets\\textures\\ui\\back.png")
+back_button_from_htp = TextureButton(630, 300, 230, 100, "assets\\textures\\ui\\back2.png")
+
+def render_text_with_spacing(text, font, color, spacing):
+    text_surface = pg.Surface((font.size(text)[0] + spacing * (len(text) - 1), font.size(text)[1]), pg.SRCALPHA)
+
+    x_offset = 0
+
+    for char in text:
+        char_surface = font.render(char, True, color)
+        text_surface.blit(char_surface, (x_offset, 0))
+        x_offset += char_surface.get_width() + spacing
+
+    return text_surface
 
 def display_rules(window):
     window.fill((135, 95, 22))
     
     rules = [
-        "Складність гри можна обрати в налаштуваннях",
-        "Темні блоки - не ламаютья, світлі можна зламати. ",
+        "Сложность игры можно настроить в настройках.",
+        "Темные блоки не ломаются. Только белые!.",
         "Правила гри: Отбивайтесь от вражеских танков защищая свою базу!",
         "Вверх - W",
         "Вниз - S",
@@ -18,11 +30,18 @@ def display_rules(window):
         "Стрелять - R"
     ]
     
+    font_path = r"fonts\tankrusbyme.otf"
+    font_size = 36
+    font = pg.font.Font(font_path, font_size)
+    
+    text_color = (255, 255, 255)
+    letter_spacing = 5
+
     y_offset = 50
     for rule in rules:
-        text = font.render(rule, True, (255, 255, 255))
+        text = render_text_with_spacing(rule, font, text_color, letter_spacing)
         window.blit(text, (50, y_offset))
-        y_offset += 50
+        y_offset += text.get_height() + 20
     
     back_button_from_htp.draw(window)
 
